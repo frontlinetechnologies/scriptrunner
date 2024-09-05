7e7482314416cc6c9c5d16c2eac0272c53c915d9
Michaël Opdebeeck <michael@mopdebeeck.be>
2024-09-05 22:55:33 +0200

    scriptrunner class

--- api-fetch/index.ts
+++ api-fetch/index.ts
@@ -1,14 +1,25 @@
 import fetch from "node-fetch";
+import Scriptrunner, { InputParams as BaseInputParams } from "..";
+
+interface ApiInputParams extends BaseInputParams {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+}
+
+type OutputParams = {
+  data: any;
+};
 
 /**
  * Script to call an API using nodejs fetch
  */
-
-async function run(params: Params) {
+async function run(params: ApiInputParams) {
   try {
-    const response = await fetch(params.url, {
+    console.log("Params::", params);
+    const response = await fetch(params.envVars.api_url, {
       method: params.method,
     });
+    const result = await response.json();
+    return result;
   } catch (err) {
     throw new Error(`Failed to fetch data: ${err}`);
   } finally {
@@ -16,20 +27,5 @@ async function run(params: Params) {
   }
 }
 
-// Input parameters
-const args = process.argv.slice(2) as unknown as Params;
-type Params = {
-  url: string;
-  method: string;
-};
-
 // Run the script
-run(args)
-  .then(() => {
-    console.log("Done");
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+new Scriptrunner<ApiInputParams, OutputParams>().run(run);
--- index.ts
+++ index.ts
@@ -1,1 +1,67 @@
-console.log("Hello via Bun!");
+import * as fs from "fs";
+import * as dotenv from "dotenv";
+
+// Define the InputParams interface
+export interface InputParams
+  extends Record<string, string | Record<string, string>> {
+  envFile: string;
+  envVars: Record<string, string>;
+}
+
+export default class Scriptrunner<
+  I extends InputParams,
+  OutputParams extends Record<string, any>
+> {
+  private inputParams: I;
+
+  constructor() {}
+
+  private extractParams(): I {
+    // Usage example
+    const encodedArgs = process.argv[2]; // Get the Base64-encoded JSON argument
+    // Decode Base64 to JSON string
+    const jsonString = Buffer.from(encodedArgs, "base64").toString("utf-8");
+
+    // Parse JSON string to Params object
+    let params: I;
+    try {
+      params = JSON.parse(jsonString) as I;
+    } catch (error) {
+      console.error("Failed to parse JSON:", error);
+      params = {} as I;
+    }
+
+    params.envVars = {};
+    if (params.envFile) {
+      dotenv.config({ path: params.envFile });
+      // Add dotenv variables to params
+      const envVars = dotenv.parse(fs.readFileSync(params.envFile));
+      params.envVars = envVars;
+    }
+
+    return params;
+  }
+
+  async run(func: (params: I) => Promise<any>): Promise<void> {
+    try {
+      this.inputParams = this.extractParams();
+      const result = await func(this.inputParams);
+      this.outputResult(result);
+      // keep running 2 seconds
+      setTimeout(() => {
+        process.exit(0);
+      }, 2000);
+    } catch (error) {
+      console.error("Error executing function:", error);
+      this.outputResult({ error: "An error occurred during execution" });
+    }
+  }
+
+  private outputResult(result: any): void {
+    const outputPath = "output.json";
+    const resultString = JSON.stringify(result, null, 2);
+
+    fs.writeFileSync(outputPath, resultString);
+    console.log(`Result written to ${outputPath}`);
+  }
+}
